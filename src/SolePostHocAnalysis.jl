33ae4a7fb840b619a85e14e4f2e345d6ba7be980
giopaglia <24519853+giopaglia@users.noreply.github.com>
2022-10-05 18:49:53 +0200

    Add rule extraction from ModalDecisionTrees.jl

--- src/SolePostHocAnalysis.jl
+++ src/SolePostHocAnalysis.jl
@@ -1,5 +1,10 @@
 module SolePostHocAnalysis
 
+using Metrics: mse
+using SoleFeatures: findcorrelation
+using SoleLogics: operators
+
+# include("rule-extraction.jl")
 # Write your package code here!
 
 end
