a3d4f69ffbe23511a1a0d910858f0c82611adefc
Michele21 <ghiottimichele12@gmail.com>
2023-08-28 21:50:29 +0200

    added hyperparameterization and minor fixes

--- src/bellatrex.jl
+++ src/bellatrex.jl
@@ -4,6 +4,7 @@ using Clustering
 using NearestNeighbors
 using LinearAlgebra: norm
 using Random
+using Hyperopt
 using ModalDecisionTrees: DForest
 
 # hyperparameter: ntrees, nclusters
@@ -14,6 +15,9 @@ function bellatrex(
     m::Union{AbstractModel,DecisionForest,DForest},
     X::AbstractLogiset, # testing dataset
     Y::AbstractVector{<:Label};
+    exec_ntrees::Vector{Float64}=[0.2,0.5,0.8],
+    exec_ndims::Vector=[2,5,nothing],
+    exec_nclusters::Vector{Int64}=[1,2,3],
     rng::AbstractRNG = MersenneTwister(1),
     kwargs...,
 )
@@ -33,57 +37,97 @@ function bellatrex(
 
     # 1) For each instance in X, we would extract the most adaptive rules for it
     for idx in 1:ndatasetinstances
+        dict_predictions = Dict()
 
-        println("Running instances $(idx)/$(ndatasetinstances)")
+        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ " *
+                "Running instances $(idx)/$(ndatasetinstances) " *
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        )
         currentinstance = slicedataset(X,[idx]; return_view=true)
 
-        # 2) Only trees that correctly predict the considered instance are considered
-        println("Concording phase in...")
-        ctrees, idxsctrees = @time concordingtrees(ftrees, currentinstance, Y[idx])
+        global ho
+        ho = @hyperopt for i = (
+                            length(exec_ntrees)*length(exec_ndims)*length(exec_nclusters)
+                        ),
+                        ntrees = exec_ntrees,
+                        ndims = exec_ndims,
+                        nclusters = exec_nclusters
 
-        # 3) Representing rules as a vector
-        println("Rule to vector phase in...")
-        rtrees, vrules = @time begin
-            rtrees = begin
-                rs = ruleset[idxsctrees]
-                rs = rs isa Vector{<:Vector{<:Any}} ? reduce(vcat,rs) : rs
-                filter(r -> first(antecedenttops(r,currentinstance)) == true, rs)
+            #println("Iteration number: $(i)")
+            #println("Parameters:")
+            #println(" - # Trees: $(ntrees)")
+            #println(" - # Dimensions: $(ndims)")
+            #println(" - # Clusters: $(nclusters)")
+            # 2) Only trees that correctly predict the considered instance are considered
+            #println("Concording phase in...")
+            ctrees, idxsctrees = concordingtrees(
+                ftrees, currentinstance, Y[idx]; ntrees=ntrees, kwargs...,
+            )
+
+            # 3) Representing rules as a vector
+            #println("Rule to vector phase in...")
+            rtrees, vrules = begin
+                rtrees = begin
+                    rs = ruleset[idxsctrees]
+                    rs = rs isa Vector{<:Vector{<:Any}} ? reduce(vcat,rs) : rs
+
+                    brs = Vector{Bool}(undef,length(rs))
+                    Threads.@threads for (i,r) in collect(enumerate(rs))
+                        brs[i] = first(antecedenttops(r,currentinstance))
+                    end
+
+                    rs[findall(brs .== true)]
+                end
+
+                (rtrees,[rule2vector(t,X) for t in rtrees])
             end
 
-            (rtrees,[rule2vector(t,X) for t in rtrees])
+            # https://juliastats.org/MultivariateStats.jl/dev/pca/
+            # 4) Vector projection (one of them):
+            #     - Principal Component Analysis (PCA)
+            #     - Multidimensional Scaling (MDS)
+            #println("Projection phase in...")
+            prules = projectionvectors(
+                hcat(vrules...),X,Y; maxoutdim=ndims, kwargs...,
+            )
+
+            # https://juliastats.org/Clustering.jl/dev/kmeans.html
+            # 5) Clustering using k-means++ method
+            #println("Clustering phase in...")
+            krules, cluster_sizes = clusteringvectors(
+                prules; rng=rng, nclusters=nclusters, kwargs...,
+            )
+
+            # 6) Surrogate model: combination between rules and instance
+            # KDTree construction: https://github.com/KristofferC/NearestNeighbors.jl
+            #println("Final phase: k-nearest neighbors in...")
+            final_rules_idx = begin
+                kdrules = KDTree(prules, Minkowski(3.5); leafsize = 40)
+                knn_idxs, _ = knn(kdrules, krules, 1, true)
+
+                vcat(knn_idxs)
+            end
+
+            # https://github.com/Klest94/Bellatrex/blob/main/code_scripts/LocalMethod_class.py row 241
+            #final_trees_idx = [idxsctrees[k] for k in final_rules_idx]
+            #localprediction(final_rules_idx,cluster_sizes)
+            local_predictions = [
+                first(apply(r,currentinstance))
+                for r in rtrees[reduce(vcat,final_rules_idx)]
+            ]
+            final_prediction =
+                bestguess(local_predictions,cluster_sizes; suppress_parity_warning=true)
+
+            dict_predictions[[ntrees,ndims,nclusters]] = final_prediction
+
+            final_prediction == Y[idx] ? 0.0 : 1.0
         end
 
-        # https://juliastats.org/MultivariateStats.jl/dev/pca/
-        # 4) Vector projection (one of them):
-        #     - Principal Component Analysis (PCA)
-        #     - Multidimensional Scaling (MDS)
-        println("Projection phase in...")
-        prules = @time projectionvectors(hcat(vrules...),X,Y; kwargs...)
-
-        # https://juliastats.org/Clustering.jl/dev/kmeans.html
-        # 5) Clustering using k-means++ method
-        println("Clustering phase in...")
-        krules, cluster_sizes = @time clusteringvectors(prules; rng=rng, kwargs...)
-
-        # 6) Surrogate model: combination between rules and instance
-        # KDTree construction: https://github.com/KristofferC/NearestNeighbors.jl
-        println("Final phase: k-nearest neighbors in...")
-        final_rules_idx = @time begin
-            kdrules = KDTree(prules, Minkowski(3.5); leafsize = 40)
-            knn_idxs, _ = knn(kdrules, krules, 1, true)
-
-            vcat(knn_idxs)
-        end
-
-        # https://github.com/Klest94/Bellatrex/blob/main/code_scripts/LocalMethod_class.py row 241
-        #final_trees_idx = [idxsctrees[k] for k in final_rules_idx]
-        #localprediction(final_rules_idx,cluster_sizes)
-        local_predictions = [first(apply(r,currentinstance)) for r in rtrees[reduce(vcat,final_rules_idx)]]
-        final_prediction = bestguess(local_predictions,cluster_sizes; suppress_parity_warning=true)
-        push!(final_predictions,final_prediction)
-
-        @show rtrees[reduce(vcat,final_rules_idx)]
-        @show final_prediction
+        best_hyperparam = ho.minimizer
+        @show best_hyperparam
+        best_prediction = dict_predictions[best_hyperparam]
+        @show best_prediction
+        push!(final_predictions,best_prediction)
     end
 
     return final_predictions
@@ -97,22 +141,27 @@ function concordingtrees(
     m::Vector{<:DecisionTree},
     X::AbstractLogiset,
     y::Label;
-    ntrees::Integer=20,
+    ntrees::AbstractFloat=0.2,
     kwargs...,
 )
-    y_preds = [apply(t,X) for t in m]
-
-    treesloss = begin
-        if y isa CLabel
-            # Classification problem: find an alternative of euclidean norm
-            y_preds .== y
-        else
-            # Regression problem: euclidean norm
-            [norm(y-y_preds[k]) for k in eachindex(y_preds)]
-        end
+    #y_preds = [apply(t,X) for t in m]
+    ntrees = Integer(ntrees * length(m))
+    y_preds = Vector{Label}(undef,length(m))
+    Threads.@threads for (i,t) in collect(enumerate(m))
+        y_preds[i] = first(apply(t,X))
     end
 
-    preselectionidxs = sortperm(treesloss)[1:ntrees]
+    preselectionidxs = begin
+        if y isa CLabel
+            # Classification problem: find an alternative of euclidean norm
+            treesloss = y_preds .== y
+            sortperm(treesloss; rev=true)[1:ntrees]
+        else
+            # Regression problem: euclidean norm
+            treesloss = [norm(y-y_preds[k]) for k in eachindex(y_preds)]
+            sortperm(treesloss)[1:ntrees]
+        end
+    end
 
     #return m[y_preds .== y]
     return (m[preselectionidxs], preselectionidxs)
@@ -177,9 +226,12 @@ function projectionvectors(
     X::AbstractLogiset,
     Y::AbstractVector{<:Label};
     projection_method::Symbol = :MDS,
+    maxoutdim::Union{Integer,Nothing} = nothing,
     kwargs...,
 )
-    maxoutdim = min(size(matrixrules)...)
+    maxoutdim = isnothing(maxoutdim) ?
+                    min(size(matrixrules)...) :
+                    min(maxoutdim,size(matrixrules)...)
 
     space = begin
         if projection_method == :MDS
@@ -212,17 +264,3 @@ function clusteringvectors(
     # .centers gets the cluster centers
     return R.centers, counts(R)
 end
-
-                    #############################################
-                    #############################################
-#=
-function localprediction(
-    final_trees_idx::Vector{Integer},
-    cluster_sizes::Vector{Integer},
-)
-    @assert length(final_trees_idx) == length(cluster_sizes) "$(final_trees_idx) !=" *
-        " $(cluster_sizes)"
-
-    bestguess
-end
-=#
