be0f10631ae72e16c59ae99a1cdd8484fd1d1b10
PasoStudio73 <paso.studio73@gmail.com>
2025-03-15 18:49:40 +0100

    listrules wrap for SoleXplorer

--- src/SolePostHoc.jl
+++ src/SolePostHoc.jl
@@ -5,6 +5,11 @@ using SoleData
 using SoleLogics
 using SoleModels
 
+# wrapper for SoleXplorer
+using DataFrames
+import SoleModels: listrules
+listrules(m, X::AbstractDataFrame, y, args...; kwargs...) = listrules(m, args...; kwargs...)
+
 # Function mse (from Metrics.jl)
 mse = (y_pred,t_true) -> (sum((y_true .- y_pred).^2) / length(y_true))
 
